03acfe57f8b7eb8035219f3b275eae9265ffc406
akorede <emiolasaheed1@gmail.com>
2023-09-11 17:20:56 +0100

    Fetch news

--- src/features/news/newsSlice.ts
+++ src/features/news/newsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { newsProp, newsStateProp } from "../../interface";
+import { fetchNews } from "../../api/newsApi";
 
 const initialState: newsStateProp = {
   news: [],
@@ -7,6 +8,16 @@ const initialState: newsStateProp = {
   error: ''
 };
 
+export const getNews = createAsyncThunk('news/getNews', async (params: {count: number, query: string}) => {
+  const {count, query} = params;
+  try {
+    const data = await fetchNews(count, query);
+    console.log(data);
+    return data
+  } catch (error) {
+    return error;
+  }
+})
 const newsSlice = createSlice({
   name: 'news',
   initialState,
