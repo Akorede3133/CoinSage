ae154a9916b382f26ecdcc8fa2f5ddb93fbe11c1
akorede <emiolasaheed1@gmail.com>
2023-09-11 13:20:27 +0100

    Create the fecth coins function

--- src/features/coins/coinsSlice.ts
+++ src/features/coins/coinsSlice.ts
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 interface coinProp {
   uuid: string,
   name: string,
@@ -21,20 +21,58 @@ interface singleCoinProp extends coinProp {
 
 }
 interface initialStateProp {
+  loading: boolean,
   coins: coinProp[],
   singleCoin: singleCoinProp,
+  error: string | undefined,
 }
 
+const url = 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2006fcddebmshb8e2fb348e572f5p13c9acjsn19e6f039cdde',
+		'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+	}
+};
+
+export const getCoins = createAsyncThunk('coins/getCoins', async () => {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+})
+
 const initialState: initialStateProp = {
+  loading: false,
   coins: [],
   singleCoin: {} as singleCoinProp,
-}
+  error: ''
+};
 
 const coinsSlice = createSlice({
   name: 'coins',
   initialState,
   reducers: {},
-  extraReducers: {}
-})
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCoins.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getCoins.fulfilled, (state, action: PayloadAction<coinProp[]> ) => {
+        state.loading = false;
+        state.coins = action.payload;
+      })
+      .addCase(getCoins.rejected, (state, action) => {
+        state.loading = false,
+        state.error =  action.error.message;
+      })
+  }
+});
 
 export default coinsSlice.reducer;
