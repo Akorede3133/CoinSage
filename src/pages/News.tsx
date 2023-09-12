feb843013328725dc872c3f152c86a5c0e52916a
akorede <emiolasaheed1@gmail.com>
2023-09-12 17:12:32 +0100

    Populate coin names for filtering

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -3,14 +3,17 @@ import { useAppSelector, useAppDispatch } from '../app/hooks';
 import { newsProp } from '../interface';
 import { getNews } from '../features/news/newsSlice';
 import NewsCard from '../components/NewsCard'
+import {AiOutlineSearch} from 'react-icons/ai';
+import {RiArrowDropDownLine} from 'react-icons/ri';
 
 const News = () => {
   const { news, loading, error } = useAppSelector((state) => state.news);
+  const { coins } = useAppSelector((state) => state.coins);
+  
   const dispatch = useAppDispatch();
-  console.log(news);
   
   useEffect(() => {
-    dispatch(getNews({count: 13, query: 'cryptocurrency'}));
+    // dispatch(getNews({count: 13, query: 'cryptocurrency'}));
   }, [])
   if (loading) {
     return <h2>Loading...</h2>
@@ -20,17 +23,27 @@ const News = () => {
   }
   return (
     <section className='min-h-screen w-[93%] mx-auto my-5'>
-      <div>
-        <select name="" id="" placeholder='select a crypto' className="w-[200px] cursor-pointer outline-blue-300 p-1 mt-5 border hover:border-blue-400 rounded-md">
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-          <option value="3h">3h</option>
-        </select>
-      </div>
+      <article>
+        <div className='flex rounded-md bg-white justify-between items-center border w-[200px] py-1 px-4 '>
+          <input 
+            type="text" 
+            name="" 
+            id="" 
+            className='border-0 bg-transparent outline-none w-[100%]'
+            placeholder='Select a crypto'
+          />
+          <div>
+            <AiOutlineSearch className="hidden" />
+            <RiArrowDropDownLine className="text-3xl text-slate-400" />
+          </div>
+        </div>
+        <ul className='bg-white grid gap-2 w-[200px] rounded-md mt-1 py-3 h-[200px] overflow-auto'>
+          {
+            coins.map((coin) => (<li className='hover:bg-slate-300 px-4 cursor-pointer'>{coin.name}</li>))
+          }
+          
+        </ul>
+      </article>
       <ul className='mt-5 grid gap-8 md:grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
       {
           news.map((news: newsProp, index) => {
