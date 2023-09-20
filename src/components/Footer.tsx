edbaf714c6e0f0237a9cc0dcea830003cebab703
akorede <emiolasaheed1@gmail.com>
2023-09-20 07:31:11 +0100

    Fix footer copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className=' bg-slate-900 p-7 text-white flex flex-col items-center'>
-      <p>Copyright &cpy; 2023 <a href="#">CoinSage Inc</a></p>
+      <p>Copyright &copy; {year} <a href="#">CoinSage Inc.</a></p>
       <p>All Rights Reserved.</p>
     </div>
   )
